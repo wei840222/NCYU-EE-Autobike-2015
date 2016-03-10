aac345a406f08b8f04da3dd643a540d134777235
imper0502 晌燁 <imper0502@outlook.com>
2016-03-10 13:59:18 +0800

    Hall library for Arduino Uno

--- libraries/Hall/Hall.cpp
+++ libraries/Hall/Hall.cpp
@@ -1,41 +1,51 @@
 //**************************************
-//*霍爾感測器類別
-//*2016-03-04
+//*霍爾感測器 library for Arduino Uno
+//*2016-03-10
 //*吳昱成
 //**************************************
-#include"Hall.h"
-class Hall{
+// For Arduino 1.0 and earlier
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
 
-  //建構元
-  void Hall() {
-  } 
-  void Hall(double lenth) {
-    pml = lenth;
-  }
-  //實體方法
-  //更新
-  void stateUpdate(){     //請把我設定成外部中斷, FALLING mode       
-    this.nowTime = millis();
+#include"Hall.h"
+//建構元
+void Hall::Hall() {
+} 
+void Hall::set(unsigned char pin, unsigned double r); 
+  m_pin = pin; 
+	m_r = r;
+  pinMode(m_pin, INPUT_PULLUP);
+  if(m_pin==2) {
+  	attachInterrupt(0, this.stateUpdate, FALLING);
+	}
+  if(m_pin==3) {
+  	attachInterrupt(1, this.stateUpdate, FALLING);
+	}
+}
+//方法
+void Hall::stateUpdate(){
+  this.nowTime = millis();
     if(this.preTime==0) {
-        this.preTime = this.nowTime;
-    }else if(this.preSpeed==0){
-        this.nowSpeed = this.pml/(this.nowTime-this.preTime)*36;    //*36 得(km/hr)
-        this.preTime = this.nowTime;
-        this.preSpeed = this.nowSpeed;
-    }else {
-        this.nowSpeed = this.pml/(this.nowTime-this.preTime)*36;
-        this.nowAcc = (this.nowSpeed-this.preSpeed)/(this.nowTime-this.preTime);
-        this.preTime = this.nowTime;
-        this.preSpeed = this.nowSpeed;
-    }
+      this.preTime = this.nowTime;
+  }else if(this.preSpeed==0){
+      this.nowSpeed = this.pml/(this.nowTime-this.preTime)*36;    //" *36 " 得(km/hr)
+      this.preTime = this.nowTime;
+      this.preSpeed = this.nowSpeed;
+  }else {
+      this.nowSpeed = this.pml/(this.nowTime-this.preTime)*36;
+      this.nowAcc = (this.nowSpeed-this.preSpeed)/(this.nowTime-this.preTime)/3.6;//" /3.6 "得(m/s) 
+      this.preTime = this.nowTime;
+      this.preSpeed = this.nowSpeed;
   }
-  //傳值
-  double getSpeed() {
-    //stateUpdate();    
-    return this.nowSpeed;
-  }
-  double getAcc() {
-    //stateUpdate();
-    return this.nowAcc;
-  }
+}
+//傳值
+double Hall::getSpeed() {  
+  return this.nowSpeed;
+}
+double Hall::getAcc() {
+	stateUpdate();
+  return this.nowAcc;
 }
--- libraries/Hall/Hall.h
+++ libraries/Hall/Hall.h
@@ -1,25 +1,41 @@
 //**************************************
-//*霍爾感測器類別
-//*2016-03-04
+//*霍爾感測器 library for Arduino Uno
+//*2016-03-10
 //*吳昱成
 //**************************************
+#ifndef Hall_h
+#define Hall_h
 
+#include <inttypes.h>
+/*
+// For Arduino 1.0 and earlier
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+*/
 class Hall{
-	//運算用常數
-	const float r = 29;				//(cm)
+public:
   //@變數
-  double preTime = 0;       //前一個時間點
-  double nowTime = 0;       //現在的時間點
+	unsigned long 
+  unsigned char m_pin;
+	unsigned double m_r = 29;				//(cm)
+  unsigned double pml = 2*PI*m_r;
+	unsigned double preTime = 0;       //前一個時間點
+  unsigned double nowTime = 0;       //現在的時間點
   double preSpeed = 0;
   double nowSpeed = 0;
   double nowAcc = 0;
-  double pml = 2*pi*r;
   //建構元
-  void Hall();
-  void Hall(double lenth);
-  //更新
+  void Hall(void);
+  //方法
+  void set(unsigned char pin, unsigned double r); 
   void stateUpdate();
-  //傳值
   double getSpeed();
   double getAcc();
-}
+protected:
+private:
+};
+
+#endif
