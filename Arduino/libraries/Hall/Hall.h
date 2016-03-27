afa53381080e962490d5593aa02878482549caa5
imper0502 晌燁 <imper0502@outlook.com>
2016-03-27 15:41:20 +0800

    Revert "刪除Libraries"
    This reverts commit 9f6c83eb83a6822ca27a5c6b9b1bdaafe7d25eab.

---
+++ Arduino/libraries/Hall/Hall.h
@@ -0,0 +1,29 @@
+//**************************************
+//*霍爾感測器
+//*2016-03-10
+//*吳昱成
+//**************************************
+#ifndef Hall_h
+#define Hall_h
+
+#include "Arduino.h"
+
+class Hall {
+public:
+	Hall(int, double);
+    void set(int, double);
+    void stateUpdate();
+    double getSpeed();
+    double getAcc();
+private:
+    int _pin;
+    double _r;
+    double _pml;
+    volatile double _preTime;       //前一個時間點
+   	volatile double _nowTime;       //現在的時間點
+    volatile double _preSpeed;
+    volatile double _nowSpeed;
+    volatile double _nowAcc;    
+};
+
+#endif
